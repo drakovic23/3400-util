e4db678cdf1ddd89f9bfe13a67aa12e965f12bf1
Deni Rakovic <deni.rakovic23@gmail.com>
2025-03-20 17:42:38 -0400

    misc

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -49,6 +49,10 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->actionHydro, &QAction::triggered, this, &MainWindow::on_HydroTriggered);
     connect(ui->actionInternetService, &QAction::triggered, this, &MainWindow::on_InternetTriggered);
 
+    //Combo box on Internet TV Page
+    QStringList inetDropDownOptions = {"TV", "Mobile Phone", "Home Phone"};
+    ui->comboBox->addItems(inetDropDownOptions);
+
 }
 
 MainWindow::~MainWindow()
@@ -56,6 +60,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//Triggers for changing the page on the stacked widget
 void MainWindow::on_NatGasTriggered()
 {
     ui->stackedWidget->setCurrentIndex(0);
